f73bb47b77daeafa383ca35ca9382cd1216a054a
JuanSanMtz <jsammtz@gmail.com>
2023-07-20 20:32:20 -0600

    DINAMICOS

---
+++ Object Repository/DINAMICOS/a_opcionMenuDYN.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_opcionMenuDYN</name>
+   <tag></tag>
+   <elementGuidId>bdf6644f-51bf-4bc3-be80-1a66828d2288</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class=&quot;left-pannel&quot;]//span[@class=&quot;group-header&quot;]//div[text() = '${textoMenu}']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
